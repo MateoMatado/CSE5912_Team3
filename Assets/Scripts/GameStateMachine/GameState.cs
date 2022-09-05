3d903e759e61f7dc9c97ad30db9672d8db4e2488
jwbrowning <jwbrowning19@gmail.com>
2022-09-04 21:47:42 -0400

    added states for state pattern

--- Assets/Scripts/GameStateMachine/GameState.cs
+++ Assets/Scripts/GameStateMachine/GameState.cs
@@ -5,5 +5,21 @@ using UnityEngine;
 public abstract class GameState
 {
     protected GameStateMachine stateMachine;
+
+    public void SwitchState(GameState newState)
+    {
+        stateMachine.CurrentState = newState;
+        Exit();
+        newState.Enter();
+    }
     
+    public virtual void Enter()
+    {
+
+    }
+
+    public virtual void Exit()
+    {
+
+    }
 }
