f84e69b2c58f5fac72882647dacfa073aa41c299
jwbrowning <jwbrowning19@gmail.com>
2022-09-06 00:47:34 -0400

    slowed down loading screen

--- Assets/Scripts/LoadingState.cs
+++ Assets/Scripts/LoadingState.cs
@@ -12,6 +12,8 @@ public class LoadingState : MonoBehaviour
     [SerializeField] private GameObject picture1;
     [SerializeField] private GameObject picture2;
     [SerializeField] private GameObject picture3;
+    [SerializeField] private float slideDuration = 2f;
+    [SerializeField] private float loadDuration = 2f;
     public Slider slider;
     public Text ValueText;
     public Text LoadingText;
@@ -31,49 +33,49 @@ public class LoadingState : MonoBehaviour
         slider.value = 0.4f;
         ValueText.text = slider.value * 100f + "%";
         LoadingText.text = "Loading Characters";
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(loadDuration / 4f);
 
         slider.value = 0.6f;
         ValueText.text = slider.value * 100f + "%";
         LoadingText.text = "Loading Weapons";
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(loadDuration / 4f);
 
         slider.value = 0.8f;
         ValueText.text = slider.value * 100f + "%";
         LoadingText.text = "Loading Map";
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(loadDuration / 4f);
 
         slider.value = 1f;
         ValueText.text = slider.value * 100f + "%";
         LoadingText.text = "Done";
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(loadDuration / 4f);
 
         /*Unity Logo*/
         var cloneUnity =Instantiate(Unity, transform.position, transform.rotation);
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(slideDuration);
         Destroy(cloneUnity);
 
         /*OSU Logo*/
         var cloneOSU = Instantiate(OSU, transform.position, transform.rotation);
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(slideDuration);
         Destroy(cloneOSU);
 
         /*CSE Logo*/
         var cloneCSE = Instantiate(CSE, transform.position, transform.rotation);
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(slideDuration);
         Destroy(cloneOSU);
 
         /*Picture*/
         var Picture1 = Instantiate(picture1, transform.position, transform.rotation);
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(slideDuration);
         Destroy(Picture1);
 
         var Picture2 = Instantiate(picture2, transform.position, transform.rotation);
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(slideDuration);
         Destroy(Picture2);
 
         var Picture3 = Instantiate(picture3, transform.position, transform.rotation);
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(slideDuration);
         
         /*Switch to main menu*/
         GameStateMachine.Instance.SwitchState(new MainMenuState());
