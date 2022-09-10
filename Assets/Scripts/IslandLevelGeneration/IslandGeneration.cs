b3b428312ff7cdebfbcc524675d4c8db535258bb
Tyler Kneffler <kneffler.3@buckeyemail.osu.edu>
2022-09-10 13:23:20 -0400

    Island Generation Cube Test

--- Assets/Scripts/IslandLevelGeneration/IslandGeneration.cs
+++ Assets/Scripts/IslandLevelGeneration/IslandGeneration.cs
@@ -13,6 +13,8 @@ public class IslandGeneration : MonoBehaviour
     [SerializeField] private int Attempts = 10;
 
     [SerializeField] private List<GameObject> Islands;
+
+    private Dictionary<GameObject,Vector2> DIslands = new Dictionary<GameObject,Vector2>();
     private enum EIslands{Empty,Filled};
     private int[,] EIslandArray;
     private GameObject[,] gameObjects;
@@ -20,6 +22,9 @@ public class IslandGeneration : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        DIslands.Add(Islands[0], new Vector2(2, 2));
+        DIslands.Add(Islands[1], new Vector2(4, 4));
+
         EIslandArray = new int[Width,Length];
         gameObjects = new GameObject[Width, Length];
         for(int i = 0; i < Width; i++)
@@ -32,10 +37,44 @@ public class IslandGeneration : MonoBehaviour
                 gameObjects[i, j].transform.SetParent(transform);
             }
         }
-        for(int i = 0; i <= Attempts; i++)
+
+        for (int i = 0; i < Attempts; i++)
         {
-            Console.WriteLine(EIslandArray[Random.Range(0,Width),Random.Range(0,Length)]);
+            Vector2 IslandSize = DIslands[Islands[Random.Range(0, Islands.Count)]];
+            Vector2 pos = new Vector2(Random.Range(0,Width), Random.Range(0,Length));
+
+            bool check = IslandCheck(pos,IslandSize);
+
+            if (check)
+            {
+                for (int x = 0; x < IslandSize.x; x++)
+                {
+                    for (int y = 0; y < IslandSize.y; y++)
+                    {
+                        GameObject.Destroy(gameObjects[(int)pos.x, (int)pos.y]);
+                        gameObjects[x, y] = GameObject.Instantiate(Islands[0]);
+                        gameObjects[x, y].transform.position = new Vector3(x, 0, y);
+                        gameObjects[x, y].transform.SetParent(transform);
+                    }
+                }
+            }
         }
-        
+
+    }
+
+    private Boolean IslandCheck(Vector2 pos, Vector2 IslandSize)
+    {
+        bool check = true;
+        for (int x = 0; x < IslandSize.x; x++)
+        {
+            for (int y = 0; y < IslandSize.y; y++)
+            {
+                if (x + (int)pos.x < Width && y + (int)pos.y < Length && EIslandArray[x+ (int)pos.x, y+ (int)pos.y] == (int)EIslands.Filled)
+                {
+                    check = false;
+                }
+            }
+        }
+        return check;
     }
 }
