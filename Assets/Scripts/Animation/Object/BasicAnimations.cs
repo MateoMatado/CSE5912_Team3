1dc83fb5b9a9a71a5085e934f6f8ee9a1b669b4b
Tyler Kneffler <kneffler.3@buckeyemail.osu.edu>
2022-09-22 12:11:59 -0400

    Animated Floaating Added to animation script

--- Assets/Scripts/Animation/Object/BasicAnimations.cs
+++ Assets/Scripts/Animation/Object/BasicAnimations.cs
@@ -8,20 +8,42 @@ public class BasicAnimations : MonoBehaviour
     [SerializeField] Boolean Rotate = false;
     [SerializeField] Vector3 RotateDirection = new Vector3(0,0,0);
     [SerializeField] Boolean Float = false;
-    [SerializeField] int FloatHeight = 0;
-    [SerializeField] int FloatSpeed = 0;
+    [SerializeField] float FloatHeight = 0;
+    [SerializeField] float FloatSpeed = 0;
 
-    Boolean FloatSwitch = false;
+    private Boolean FloatSwitch = false;
+    private Vector3 InitPos;
+    void Start()
+    {
+        InitPos = transform.position;
+    }
     void Update()
     {
         if (Rotate)
         {
-            gameObject.transform.Rotate(RotateDirection);
+           RotateObject();
         }
         if (Float)
         {
-            gameObject.transform.position = gameObject.transform.localPosition + new Vector3(0, FloatSpeed, 0);
+            FloatObject();
         }
 
+    }
+    private void RotateObject()
+    {
+        gameObject.transform.Rotate(RotateDirection);
+    }
+    private void FloatObject()
+    {
+        if (FloatSwitch)
+        {
+            gameObject.transform.position = gameObject.transform.position + new Vector3(0, FloatSpeed, 0);
+        }
+        else
+        {
+            gameObject.transform.position = gameObject.transform.position - new Vector3(0, FloatSpeed, 0);
+        }
+        if(gameObject.transform.position.y > FloatHeight + InitPos.y) { FloatSwitch = false;}
+        else if(gameObject.transform.position.y < InitPos.y){ FloatSwitch = true;}
     }
 }
