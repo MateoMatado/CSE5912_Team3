1929eb6aad29ccc7d163d6c35a4cbc3fb744ff8a
jwbrowning <jwbrowning19@gmail.com>
2022-09-23 13:47:07 -0400

    added player states and state manager monobehavior

---
+++ Assets/Scripts/Player/PlayerStateManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Team3.Player 
+{
+    public class PlayerStateManager : MonoBehaviour
+    {
+        private PlayerStateMachine stateMachine;
+        private bool leftArmActive = false, rightArmActive = false;
+
+        void Awake() 
+        {
+            stateMachine = new PlayerStateMachine();
+
+            Events.EventsPublisher.Instance.SubscribeToEvent("LeftArmActivate", (object sender, object data) => { UpdateLeftArm(true); });
+            Events.EventsPublisher.Instance.SubscribeToEvent("LeftArmDeactivate", (object sender, object data) => { UpdateLeftArm(false); });
+            Events.EventsPublisher.Instance.SubscribeToEvent("RightArmActivate", (object sender, object data) => { UpdateRightArm(true); });
+            Events.EventsPublisher.Instance.SubscribeToEvent("RightArmDeactivate", (object sender, object data) => { UpdateRightArm(false); });
+        }
+
+
+        private void UpdateLeftArm(bool active)
+        {
+            leftArmActive = active;
+            UpdateStateWithArms();
+        }
+
+        private void UpdateRightArm(bool active)
+        {
+            rightArmActive = active;
+            UpdateStateWithArms();
+        }
+
+        private void UpdateStateWithArms()
+        {
+            if ((leftArmActive || rightArmActive) && stateMachine.CurrentState is DefaultPlayerState)
+            {
+                stateMachine.SwitchState(PlayerStateMachine.IKState);
+            }
+            else if ((!leftArmActive && !rightArmActive) && stateMachine.CurrentState is IKState)
+            {
+                stateMachine.SwitchState(PlayerStateMachine.DefaultState);
+            }
+        }
+    }
+}
