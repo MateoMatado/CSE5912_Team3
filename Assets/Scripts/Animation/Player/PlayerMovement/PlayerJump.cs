5fb4feba356587498e5ab3b28630d1fc5f31c197
mfawcett <mfawcett928@gmail.com>
2022-09-28 02:51:35 -0400

    Jumping now only works when you're on the ground

--- Assets/Scripts/Animation/Player/PlayerMovement/PlayerJump.cs
+++ Assets/Scripts/Animation/Player/PlayerMovement/PlayerJump.cs
@@ -7,13 +7,20 @@ namespace Team3.PlayerMovement
     public class PlayerJump : MonoBehaviour
     {
         [SerializeField] float jumpHeight;
+        [SerializeField] float rayLength;
+        [SerializeField] Transform bottom;
+
+        Ray ray;
 
         Rigidbody body;
+        bool jumped = false;
 
         void Start()
         {
             Events.EventsPublisher.Instance.SubscribeToEvent("PlayerJump", OnJump);
             body = GetComponent<Rigidbody>();
+            Debug.Log("Casting ray at: " + bottom.position);
+            StartCoroutine(CheckGround());
         }
 
         private void OnDestroy()
@@ -23,7 +30,30 @@ namespace Team3.PlayerMovement
 
         void OnJump(object sender, object data)
         {
-            body.velocity += new Vector3(0, Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight), 0);
+            ray = new Ray(bottom.position, new Vector3(0, -1, 0));
+            if (Physics.Raycast(ray, rayLength))
+            {
+                body.velocity = new Vector3(body.velocity.x, Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight), body.velocity.z);
+                jumped = true;
+            }
+        }
+
+        private IEnumerator CheckGround()
+        {
+            while (true)
+            {
+                while (jumped)
+                {
+                    ray = new Ray(bottom.position, new Vector3(0, -1, 0));
+                    if (Physics.Raycast(ray, rayLength) && body.velocity.y <= 0)
+                    {
+                        jumped = false;
+                        Events.EventsPublisher.Instance.PublishEvent("PlayerGrounded", null, null);
+                    }
+                    yield return null;
+                }
+                while (!jumped) { yield return null; }
+            }
         }
     }
 }
