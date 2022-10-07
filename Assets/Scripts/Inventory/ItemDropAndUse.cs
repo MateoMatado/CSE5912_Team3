4c273ccb7991fbcadb6afb7841b14b9a9a45c0ce
EricChen9000 <chen.9000@osu.edu>
2022-10-06 22:49:44 -0400

    HUD Layout
    Have the health bar and short cut key for item

--- Assets/Scripts/Inventory/ItemDropAndUse.cs
+++ Assets/Scripts/Inventory/ItemDropAndUse.cs
@@ -10,8 +10,10 @@ public class ItemDropAndUse : MonoBehaviour, IPointerClickHandler
 
     public Text name;
     public Text Amount;
+    public Image Icon;
     public GameObject UseButton;
     public GameObject DropButton;
+    public GameObject EquipButton;
 
     public void DropMenu()
     {
@@ -20,6 +22,10 @@ public class ItemDropAndUse : MonoBehaviour, IPointerClickHandler
     public void UseMenu()
     {
         InventoryManager.Instance.UseWithNumberMenu(name);
+    }
+    public void EquipQuickItem()
+    {
+        InventoryManager.Instance.EquipItem(name, Icon, Amount);
     }
 
     public void Use()
@@ -39,6 +45,7 @@ public class ItemDropAndUse : MonoBehaviour, IPointerClickHandler
         {
             UseButton.SetActive(true);
             DropButton.SetActive(true);
+            EquipButton.SetActive(true);
         }
     }
 }
