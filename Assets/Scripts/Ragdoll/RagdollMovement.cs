fcbc63b4509154f59578f2ace5ff8e3c0ce3967b
mfawcett <mfawcett928@gmail.com>
2022-10-11 00:37:05 -0400

    Created physics based animation with configurable joints

--- Assets/Scripts/Ragdoll/RagdollMovement.cs
+++ Assets/Scripts/Ragdoll/RagdollMovement.cs
@@ -6,16 +6,54 @@ namespace Team3.Ragdoll
 {
     public class RagdollMovement : MonoBehaviour
     {
-        // Start is called before the first frame update
+        [SerializeField] Animator anim;
+        [SerializeField] GameObject ragRoot;
+        [SerializeField] GameObject animRoot;
+
+        private Transform[] bones;
+        private ConfigurableJoint[] joints;
+        private Quaternion[] initialJointRotations;
+
         void Start()
         {
+            joints = ragRoot.GetComponentsInChildren<ConfigurableJoint>();
+            TransformStore[] stores = animRoot.GetComponentsInChildren<TransformStore>();
 
+            initialJointRotations = new Quaternion[joints.Length];
+            for (int i = 0; i < joints.Length; i++)
+            {
+                initialJointRotations[i] = joints[i].transform.localRotation;
+            }
+
+            bones = new Transform[stores.Length];
+            for (int i = 0; i < stores.Length; i++)
+            {
+                bones[i] = stores[i].transform;
+            }
         }
 
-        // Update is called once per frame
-        void Update()
+        void FixedUpdate()
         {
+            for (int i = 0; i < joints.Length; i++)
+            {
+                // Quaternion code used from ConfigurableJointExtensions.cs on GitHub
+                // Written by mstevenson
+                Vector3 right = joints[i].axis;
+                Vector3 forward = Vector3.Cross(right, joints[i].secondaryAxis).normalized;
+                Vector3 up = Vector3.Cross(forward, right).normalized;
+                Quaternion worldToJointSpace = Quaternion.LookRotation(forward, up);
 
+                Quaternion resultRotation = Quaternion.Inverse(worldToJointSpace);
+
+                //Debug.Log(bones[i + 1].localRotation);
+                //Debug.Log(initialJointRotations[i]);
+                resultRotation *= Quaternion.Inverse(bones[i + 1].localRotation) * initialJointRotations[i];
+                //resultRotation *= initialJointRotations[i] * Quaternion.Inverse(bones[i + 1].localRotation);
+                resultRotation *= worldToJointSpace;
+
+                joints[i].targetRotation = resultRotation;
+                //joints[i].targetRotation = Quaternion.Euler(45, 45, 45);
+            }
         }
     }
 }
---
+++ Assets/Scripts/Ragdoll/TransformStore.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Team3.Ragdoll
+{
+    // Required to inform the ragdoll system that this is a transform it actually wants
+    public class TransformStore : MonoBehaviour { }
+}
