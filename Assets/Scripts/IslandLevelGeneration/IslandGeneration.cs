f3d40e6c5883e4ff7c287624122b3d1df36c16a6
Tyler Kneffler <kneffler.3@buckeyemail.osu.edu>
2022-10-15 11:36:34 -0400

    Added Bias To island Gen

--- Assets/Scripts/IslandLevelGeneration/IslandGeneration.cs
+++ Assets/Scripts/IslandLevelGeneration/IslandGeneration.cs
@@ -6,6 +6,7 @@ using Random = UnityEngine.Random;
 
 public class IslandGeneration : MonoBehaviour
 {
+    [SerializeField] private int Bias = 3;
     [SerializeField] private int Width = 100;
     [SerializeField] private int Length = 100;
     [SerializeField] private int Height = 50;
@@ -37,60 +38,73 @@ public class IslandGeneration : MonoBehaviour
     {
         EIslandArray = new int[Width, Length];
         gameObjects = new GameObject[Width, Length];
-        
-        for (int i = 0; i < Attempts; i++)
+        int BiasDec = Bias;
+        while (BiasDec >= 0)
         {
-            int IslRand = Random.Range(0, Islands.Count);
-            Vector2 IslandSize = DIslands[Islands[IslRand]];
-            Vector2 pos = new Vector2(Random.Range(0, Width), Random.Range(0, Length));
-            float rot = Random.Range(0, 4);
-
-            bool check = IslandCheck(pos, IslandSize);
-            bool rotcheck = false;
-
-            if (check)
+            for (int i = 0; i < Attempts; i++)
             {
-                gameObjects[(int)pos.x, (int)pos.y] = GameObject.Instantiate(Islands[IslRand]);
-                if (rot == 0)
+                int IslRand = Random.Range(0, Islands.Count);
+                for (int j = 0; j < Bias; j++)
                 {
-                    gameObjects[(int)pos.x, (int)pos.y].transform.Rotate(new Vector3(0, 90, 0), Space.Self);
-                    rotcheck = true;
+                    int IslRand2 = Random.Range(0, Islands.Count - 2);
+                    if (IslRand2 > IslRand)
+                    {
+                        IslRand = IslRand2;
+                    }
                 }
-                else if (rot == 1)
-                {
-                    gameObjects[(int)pos.x, (int)pos.y].transform.Rotate(new Vector3(0, 180, 0), Space.Self);
-                }
-                else if (rot == 2)
-                {
-                    gameObjects[(int)pos.x, (int)pos.y].transform.Rotate(new Vector3(0, -90, 0), Space.Self);
-                    rotcheck = true;
-                }
-                gameObjects[(int)pos.x, (int)pos.y].transform.position = new Vector3((int)pos.x * Spacing, Random.Range(100, RandomHeightLimit), (int)pos.y * Spacing);
-                gameObjects[(int)pos.x, (int)pos.y].transform.SetParent(transform);
 
-                if (rotcheck) {
-                    for (int x = 0; x < IslandSize.x; x++)
-                    {
-                        for (int y = 0; y < IslandSize.y; y++)
-                        {
-                            EIslandArray[x + (int)pos.x, y + (int)pos.y] = (int)EIslands.Filled;
-                        }
-                    }
-                }
-                else
+                Vector2 IslandSize = DIslands[Islands[IslRand]];
+                Vector2 pos = new Vector2(Random.Range(0, Width), Random.Range(0, Length));
+                float rot = Random.Range(0, 4);
+
+                bool check = IslandCheck(pos, IslandSize);
+                bool rotcheck = false;
+
+                if (check)
                 {
-                    for (int x = 0; x < IslandSize.y; x++)
+                    gameObjects[(int)pos.x, (int)pos.y] = GameObject.Instantiate(Islands[IslRand]);
+                    if (rot == 0)
                     {
-                        for (int y = 0; y < IslandSize.x; y++)
+                        gameObjects[(int)pos.x, (int)pos.y].transform.Rotate(new Vector3(0, 90, 0), Space.Self);
+                        rotcheck = true;
+                    }
+                    else if (rot == 1)
+                    {
+                        gameObjects[(int)pos.x, (int)pos.y].transform.Rotate(new Vector3(0, 180, 0), Space.Self);
+                    }
+                    else if (rot == 2)
+                    {
+                        gameObjects[(int)pos.x, (int)pos.y].transform.Rotate(new Vector3(0, -90, 0), Space.Self);
+                        rotcheck = true;
+                    }
+                    gameObjects[(int)pos.x, (int)pos.y].transform.position = new Vector3((int)pos.x * Spacing, Random.Range(100, RandomHeightLimit), (int)pos.y * Spacing);
+                    gameObjects[(int)pos.x, (int)pos.y].transform.SetParent(transform);
+
+                    if (rotcheck)
+                    {
+                        for (int x = 0; x < IslandSize.x; x++)
                         {
-                            EIslandArray[x + (int)pos.x, y + (int)pos.y] = (int)EIslands.Filled;
+                            for (int y = 0; y < IslandSize.y; y++)
+                            {
+                                EIslandArray[x + (int)pos.x, y + (int)pos.y] = (int)EIslands.Filled;
+                            }
                         }
                     }
+                    else
+                    {
+                        for (int x = 0; x < IslandSize.y; x++)
+                        {
+                            for (int y = 0; y < IslandSize.x; y++)
+                            {
+                                EIslandArray[x + (int)pos.x, y + (int)pos.y] = (int)EIslands.Filled;
+                            }
+                        }
+                    }
+
                 }
-                
             }
+            BiasDec--;
         }
-        
     }
 
     private Boolean IslandCheck(Vector2 pos, Vector2 IslandSize)
