e9961b7a3ab37efa0d0a0eb19d3c5f2dae6866bf
Tyler Kneffler <kneffler.3@buckeyemail.osu.edu>
2022-11-29 21:01:57 -0500

    Fixed island gen bugs

--- Assets/Scripts/IslandLevelGeneration/IslandGeneration.cs
+++ Assets/Scripts/IslandLevelGeneration/IslandGeneration.cs
@@ -48,7 +48,7 @@ public class IslandGeneration : MonoBehaviour, IData
         DIslands.Add(Islands[4].name.ToString(), new Vector2(800, 800));
         DIslands.Add(Islands[5].name.ToString(), new Vector2(800, 800));
         DIslands.Add(Islands[6].name.ToString(), new Vector2(800, 600));
-        DIslands.Add(Islands[7].name.ToString(), new Vector2(350, 350));
+        DIslands.Add(Islands[7].name.ToString(), new Vector2(800, 800));
         if (Island.Count != 0)
         {
             Debug.Log("Total Island: " + Island.Count);
